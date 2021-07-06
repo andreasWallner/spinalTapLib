27f81f680daa74545aa9d3a87d3913ba3df98184
Andreas Wallner <A.Wallner@innovative-solutions.at>
2021-07-06 02:36:23 +0200

    Complete IOMux

--- external/spinaltap/include/spinaltap/iomux/iomux.hpp
+++ external/spinaltap/include/spinaltap/iomux/iomux.hpp
@@ -8,12 +8,13 @@ namespace spinaltap::iomux {
 class iomux {
 private:
   device &device_;
-  uint32_t baseAddress_;
+  uint32_t base_address_;
 
 public:
-  iomux(device &device) : device_(device) {}
+  iomux(device &device, uint32_t base_address)
+      : device_(device), base_address_(base_address) {}
 
-  void connect(uint8_t output, uint8_t input);
+  void connect(uint8_t input, uint8_t output);
 };
 
 } // namespace spinaltap::iomux
--- external/spinaltap/src/iomux.cpp
+++ external/spinaltap/src/iomux.cpp
@@ -3,9 +3,9 @@
 
 namespace spinaltap::iomux {
 
-void iomux::connect(uint8_t output, uint8_t input) {
-  auto reg = device_.readRegister(baseAddress_ + registers::route(output));
-  device_.writeRegister(baseAddress_ + registers::route(output),
+void iomux::connect(uint8_t input, uint8_t output) {
+  auto reg = device_.readRegister(base_address_ + registers::route(output));
+  device_.writeRegister(base_address_ + registers::route(output),
                         reg & ~registers::route_sel_msk(output) |
                             (input << registers::route_sel_pos(output)));
 }
